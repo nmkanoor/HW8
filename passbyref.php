9aba285650bf442f99b8040d93ded58a9ce7fdd8
Nikhil Kanoor <nk352@afsconnect2.njit.edu>
2016-11-20 15:58:21 -0500

    PASSBYVALUENOTREF

--- passbyref.php
+++ passbyref.php
@@ -1,12 +1,11 @@
 <?php
-function pass_by_value($param) {
-	push_array($param, 4, 5);
+$it = 'Larry';
+function say_hello(&$thing) {
+	echo "Hello, $thing";
+	$thing = 'World';
 }
-$ar = array(1,2,3);
-pass_by_value($ar);
-foreach($ar as $elem) {
-	print "<br>$elem";
-}
+say_hello($it);
+say_hello($it);
 /*The program name should be pass by values but I'm not sure how to rename it from terminal*/
 ?>
 
